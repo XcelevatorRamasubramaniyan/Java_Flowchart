79ede8e94e8a2a7ba130fe7a0c82edaa1e8c68d8
XcelevatorRamasubramaniyan <Ramasubramaniyan.2521@xcelevate.org>
2025-07-24 09:20:07 +0530

    FCP001

---
+++ FCP001.java
@@ -0,0 +1,10 @@
+public class FCP001{
+public static void main(String[]args){
+int a=25;
+String b="January";
+int c=2005;
+System.out.println(a);
+System.out.println(b);
+System.out.println(c);
+}
+}

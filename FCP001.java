9aaca5033e4aab492f1e11e18555b6e2b9a98404
XcelevatorRamasubramaniyan <Ramasubramaniyan.2521@xcelevate.org>
2025-07-30 10:22:29 +0530

    Update FCP001.java

--- FCP001.java
+++ FCP001.java
@@ -1,10 +1,18 @@
-public class FCP001{
-public static void main(String[]args){
-int a=25;
-String b="January";
-int c=2005;
-System.out.println(a);
-System.out.println(b);
-System.out.println(c);
-}
+import java.util.Scanner;
+
+public class FCP001 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int a = input.nextInt();
+        input.nextLine(); 
+        String b = input.nextLine();
+        int c = input.nextInt();
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+
+        input.close();
+    }
 }
